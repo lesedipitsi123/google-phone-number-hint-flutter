2df309d249ad04458f1d691d4d36388b3412191e
lesedipitsi123 <lesedipitsi@gmail.com>
2024-02-18 18:43:16 +0200

    adding dimension spacers

--- lib/common/dimens.dart
+++ lib/common/dimens.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
 const spacerRow8px = SizedBox(width: 8.0);
+const spacerRow16px = SizedBox(width: 8.0);
+const spacerRow24px = SizedBox(width: 24.0);
+const spacerRow48px = SizedBox(width: 48.0);
+
+const spacerColumn8px = SizedBox(height: 8.0);
 const spacerColumn16px = SizedBox(height: 16.0);
+const spacerColumn24px = SizedBox(height: 24.0);
+const spacerColumn48px = SizedBox(height: 48.0);
