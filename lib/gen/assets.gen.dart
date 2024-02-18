35a7d014f610321968380fb12c9a88c1b7f10c86
lesedipitsi123 <lesedipitsi@gmail.com>
2024-02-18 13:46:46 +0200

    add packages

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -7,6 +7,18 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/google-logo.svg
+  String get googleLogo => 'assets/icons/google-logo.svg';
+
+  /// List of all assets
+  List<String> get values => [googleLogo];
+}
+
 class Assets {
   Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
 }
