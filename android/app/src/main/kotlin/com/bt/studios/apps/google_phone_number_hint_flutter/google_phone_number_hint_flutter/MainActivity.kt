79b4a9652dc51e466d02e2dfc5f9b64a89c5f571
lesedipitsi123 <lesedipitsi@gmail.com>
2024-02-18 13:23:44 +0200

    add google phone number hint platform channel

--- android/app/src/main/kotlin/com/bt/studios/apps/google_phone_number_hint_flutter/google_phone_number_hint_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/bt/studios/apps/google_phone_number_hint_flutter/google_phone_number_hint_flutter/MainActivity.kt
@@ -1,6 +1,26 @@
 package com.bt.studios.apps.google_phone_number_hint_flutter.google_phone_number_hint_flutter
 
+import androidx.activity.ComponentActivity
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+    }
+
+    private fun handlePhoneHintMethodChannel(flutterEngine: FlutterEngine) {
+        val handler = PhoneHintMethodChannelHandler(this.activity as ComponentActivity)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, phoneHintChannel)
+            .setMethodCallHandler(handler)
+    }
+
+    companion object {
+        private const val phoneHintChannel =
+            "com.bt.studios.apps.google_phone_number_hint_flutter.google_phone_number_hint_flutter/phone_hint"
+    }
 }
+
+
---
+++ android/app/src/main/kotlin/com/bt/studios/apps/google_phone_number_hint_flutter/google_phone_number_hint_flutter/PhoneHintMethodChannelHandler.kt
@@ -0,0 +1,63 @@
+package com.bt.studios.apps.google_phone_number_hint_flutter.google_phone_number_hint_flutter
+
+import android.app.PendingIntent
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.identity.GetPhoneNumberHintIntentRequest
+import com.google.android.gms.auth.api.identity.Identity
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+
+class PhoneHintMethodChannelHandler(private val activity: ComponentActivity) :
+    MethodChannel.MethodCallHandler {
+    private lateinit var phoneNumber: String
+    private val request: GetPhoneNumberHintIntentRequest =
+        GetPhoneNumberHintIntentRequest.builder().build()
+    private val phoneNumberHintIntentResultLauncher = activity.registerForActivityResult(
+        ActivityResultContracts.StartIntentSenderForResult()
+    ) { result ->
+        try {
+            phoneNumber = Identity.getSignInClient(activity).getPhoneNumberFromIntent(result.data)
+        } catch (e: Exception) {
+            Log.e(TAG, "Phone Number Hint failed")
+        }
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        when (call.method) {
+            "getPhoneNumberHint" -> {
+                requestPhoneHint()
+
+                result.success(phoneNumber)
+            }
+
+            else -> {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun requestPhoneHint() {
+        Identity.getSignInClient(activity)
+            .getPhoneNumberHintIntent(request)
+            .addOnSuccessListener { result: PendingIntent ->
+                try {
+                    phoneNumberHintIntentResultLauncher.launch(
+                        IntentSenderRequest.Builder(result).build()
+                    )
+                } catch (e: Exception) {
+                    Log.e(TAG, "Launching the PendingIntent failed")
+                }
+            }
+            .addOnFailureListener {
+                Log.e(TAG, "Phone Number Hint failed")
+            }
+    }
+
+
+    companion object {
+        private const val TAG = "PhoneHintMethodChannel"
+    }
+}
