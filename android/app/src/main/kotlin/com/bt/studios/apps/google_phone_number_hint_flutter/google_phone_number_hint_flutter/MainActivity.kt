6aab9bc35e46a67b1fdba58eda46ae4cafae8fcc
lesedipitsi123 <lesedipitsi@gmail.com>
2024-02-18 12:17:15 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/bt/studios/apps/google_phone_number_hint_flutter/google_phone_number_hint_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bt.studios.apps.google_phone_number_hint_flutter.google_phone_number_hint_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
