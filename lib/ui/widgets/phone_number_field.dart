4e34e80d3053e726c0e136b1d8a5e0aed0cbf4b0
lesedipitsi123 <lesedipitsi@gmail.com>
2024-02-18 16:27:31 +0200

    update text input styling

--- lib/ui/widgets/phone_number_field.dart
+++ lib/ui/widgets/phone_number_field.dart
@@ -7,22 +7,23 @@ class PhoneNumberField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
+    return TextField(
       controller: controller,
       keyboardType: TextInputType.phone,
       cursorColor: Theme.of(context).colorScheme.inverseSurface,
-      maxLength: 20,
       style: Theme.of(context)
           .textTheme
           .bodyMedium
-          ?.copyWith(fontWeight: FontWeight.bold),
+          ?.copyWith(fontWeight: FontWeight.w500),
       decoration: InputDecoration(
+        prefixIcon: Icon(Icons.phone,
+            size: 20.0, color: Theme.of(context).colorScheme.primary),
         fillColor: Theme.of(context).colorScheme.surfaceVariant,
         filled: true,
-        icon: const Icon(Icons.phone),
         labelText: "Phone number",
-        labelStyle:
-            TextStyle(color: Theme.of(context).colorScheme.inverseSurface),
+        labelStyle: Theme.of(context).textTheme.bodyMedium?.copyWith(
+            fontWeight: FontWeight.normal,
+            color: Theme.of(context).colorScheme.inverseSurface),
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
         ),
