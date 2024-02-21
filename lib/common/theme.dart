471221099bbbfb08d5956590c6c7dc7a43f94dc8
lesedipitsi123 <lesedipitsi@gmail.com>
2024-02-21 18:14:55 +0200

    remove unused themes

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -21,50 +21,6 @@ class AppTheme {
         )),
     dividerTheme:
         const DividerThemeData(thickness: 0.8, color: Color(0xfff5f6f8)),
-    tabBarTheme: const TabBarTheme(
-      dividerHeight: 0.0,
-      indicatorSize: TabBarIndicatorSize.tab,
-      indicatorColor: Color(0xff1A1B1E),
-      indicator: BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            color: Color(0xFF00629E),
-            width: 2.0,
-          ),
-        ),
-      ),
-      labelColor: Color(0xff1A1B1E),
-      unselectedLabelColor: Color(0xffC4C6CF),
-    ),
-    bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      type: BottomNavigationBarType.fixed,
-      elevation: 0.0,
-      showSelectedLabels: true,
-      showUnselectedLabels: true,
-      backgroundColor: Colors.white,
-      selectedItemColor: const Color(0xFF00629E),
-      unselectedItemColor: const Color(0xff555F71).withOpacity(0.5),
-    ),
-    navigationBarTheme: NavigationBarThemeData(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        height: 58.0,
-        indicatorColor: Colors.transparent,
-        labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
-        labelTextStyle: MaterialStateProperty.all(const TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 12.0,
-            color: Color(0xff1A1B1E)))),
-    bottomSheetTheme: const BottomSheetThemeData(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            top: Radius.circular(12.0),
-          ),
-        ),
-        backgroundColor: Colors.white,
-        modalElevation: 0.0,
-        modalBackgroundColor: Colors.white,
-        elevation: 0.0),
     scaffoldBackgroundColor: const Color(0x0fffffff),
     colorScheme: AppColors.lightScheme,
   );
@@ -85,47 +41,6 @@ class AppTheme {
           )),
       dividerTheme:
           const DividerThemeData(thickness: 0.8, color: Color(0xff22252A)),
-      tabBarTheme: const TabBarTheme(
-        dividerHeight: 0.0,
-        indicatorSize: TabBarIndicatorSize.tab,
-        indicatorColor: Color(0xffE3E2E6),
-        indicator: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(
-              color: Color(0xFF00629E),
-              width: 2.0,
-            ),
-          ),
-        ),
-        labelColor: Color(0xffE3E2E6),
-        unselectedLabelColor: Color(0xff43474E),
-      ),
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        type: BottomNavigationBarType.fixed,
-        elevation: 0.0,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        backgroundColor: const Color(0xff1A1B1E),
-        selectedItemColor: const Color(0xFF00629E),
-        unselectedItemColor: const Color(0xffBDC7DC).withOpacity(0.5),
-      ),
-      navigationBarTheme: NavigationBarThemeData(
-          backgroundColor: const Color(0xff1A1B1E),
-          elevation: 0.0,
-          indicatorColor: Colors.transparent,
-          labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
-          labelTextStyle: MaterialStateProperty.all(const TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 12.0,
-              color: Color(0xffE3E2E6)))),
-      bottomSheetTheme: const BottomSheetThemeData(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(12.0),
-            ),
-          ),
-          backgroundColor: Color(0xff121317),
-          elevation: 0.0),
       scaffoldBackgroundColor: const Color(0xff121317),
       colorScheme: AppColors.darkScheme);
 
